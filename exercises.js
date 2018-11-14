ac8d9e287ebfc60c78fe07cb080ed5bf193ac92d
Tyler Taydus <tylertaydus4444@gmail.com>
2018-11-14 13:57:51 -1000

    4

--- exercises.js
+++ exercises.js
@@ -68,12 +68,18 @@ function leaders(person){
 
 leaders(presidents);
 
-
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
 
+var stringOfNumbers = '';
+
+for(var i = 10; i<21; i++){
+   stringOfNumbers += i;
+}
+
+console.log(stringOfNumbers);
 
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array. 
