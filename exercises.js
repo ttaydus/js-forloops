539dd48e76cd5711f7377ec4d34f105da3eae80f
Tyler Taydus <tylertaydus4444@gmail.com>
2018-11-14 13:26:31 -1000

    2

--- exercises.js
+++ exercises.js
@@ -9,7 +9,7 @@ Write a for-loop that will iterate through 20 numbers (starting at 1 and ending 
 "Now serving 20."
 */
 
-for(var i = 0; i<21; i++){
+for(var i = 1; i<21; i++){
    console.log('Now serving ' + i);
 }
 
@@ -24,6 +24,11 @@ This week's chart buster is: '24K Magic.'
 */
 
 var topFive = ["Closer", "Starboy", "I Feel It Coming", "Let Me Love You", "24K Magic"];
+
+for(var i = 0; i<topFive.length; i++){
+   console.log('This week\'s chart buster is: ' + topFive[i]);
+}
+
 
 
 /* 3) Dead Presidents
